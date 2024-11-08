7b9da9388c57f203e552096e01679b24ba6e3c5d
Herman <132596813+HermanTariq@users.noreply.github.com>
2024-11-09 04:18:49 +0500

    4

--- backend/server/server.js
+++ backend/server/server.js
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello from the Frontend 3!</h1>
+      <h1>Hello from the Frontend 4!</h1>
       <p>Backend Message: {message}</p>
     </div>
   );

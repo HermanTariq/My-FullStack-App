6d64b13e93766593a372f12bed9be0e2787829cb
Herman <132596813+HermanTariq@users.noreply.github.com>
2024-11-10 15:54:42 +0500

    Final Check

--- backend/server/server.js
+++ backend/server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from the backend!7" });
+  res.json({ message: "Hello from the backend!Check" });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello from the Frontend 13!</h1>
+      <h1>Hello from the Frontend Check!</h1>
       <p>Backend Message: {message}</p>
     </div>
   );

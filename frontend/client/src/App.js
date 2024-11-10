486d0d963b4a544426c02afe7d89195b2cc40e9b
Herman <132596813+HermanTariq@users.noreply.github.com>
2024-11-10 15:19:18 +0500

    12.99

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello from the Frontend 12!</h1>
+      <h1>Hello from the Frontend 13!</h1>
       <p>Backend Message: {message}</p>
     </div>
   );

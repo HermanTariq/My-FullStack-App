0fe751ac40b5ab17e16d37173d0cdade7df86494
Herman <132596813+HermanTariq@users.noreply.github.com>
2024-11-10 14:48:48 +0500

    11.6

--- backend/server/server.js
+++ backend/server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: "Hello from the backend!5" });
+  res.json({ message: "Hello from the backend!6" });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
